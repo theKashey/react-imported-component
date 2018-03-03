afcbcfd32a6e61e823ba08992edc07a97a328462
Anton Korzunov <thekashey@gmail.com>
2018-03-03 19:23:51 +1100

    better imports support

--- src/babel.js
+++ src/babel.js
@@ -41,44 +41,37 @@ export default function ({types: t, template}) {
     inherits: syntax,
 
     visitor: {
-      Import({parentPath}, {file}) {
-        if (visitedNodes.has(parentPath.node)) {
-          return;
-        }
-
-        const localFile = file.opts.filename;
-        const newImport = parentPath.node;
-        const importName = parentPath.get('arguments')[0].node.value;
-        const requiredFile = encipherImport(resolveImport(importName, localFile));
-
-        let replace = null;
-        if(parentPath.parentPath.type==='ArrowFunctionExpression') {
-          replace = importCallRegistration({
-            MARK: t.stringLiteral("imported-component"),
-            FILE: t.stringLiteral(requiredFile),
-            IMPORT: newImport
-          });
-
-          hasImports[localFile] = true;
-          visitedNodes.set(newImport, true);
-
-          parentPath.parentPath.replaceWith(replace);
-        }
-
-        if(parentPath.parentPath.type==='ReturnStatement') {
-          replace = importRegistration({
-            MARK: t.stringLiteral("imported-component"),
-            FILE: t.stringLiteral(requiredFile),
-            IMPORT: newImport
-          });
-
-          hasImports[localFile] = true;
-          visitedNodes.set(newImport, true);
-
-          parentPath.replaceWith(replace);
-        }
-      },
+      // using program to replace imports before "dynamic-import-node"
+      // see: https://jamie.build/babel-plugin-ordering.html
       Program: {
+        enter(programPath, {file}) {
+          programPath.traverse({
+            Import({parentPath}) {
+              if (visitedNodes.has(parentPath.node)) {
+                return;
+              }
+
+              const localFile = file.opts.filename;
+              const newImport = parentPath.node;
+              const importName = parentPath.get('arguments')[0].node.value;
+              const requiredFile = encipherImport(resolveImport(importName, localFile));
+
+              let replace = null;
+
+              replace = importRegistration({
+                MARK: t.stringLiteral("imported-component"),
+                FILE: t.stringLiteral(requiredFile),
+                IMPORT: newImport
+              });
+
+              hasImports[localFile] = true;
+              visitedNodes.set(newImport, true);
+
+              parentPath.replaceWith(replace);
+            }
+          });
+        },
+
         exit({node}, {file}) {
           if (!hasImports[file.opts.filename]) return;
 
