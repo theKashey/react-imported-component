2999fa1e74ac08f437a2a7d62b01201517eb7323
Anton Korzunov <thekashey@gmail.com>
2018-05-16 15:17:45 +1000

    better example

--- examples/SSR/typescript-react/app/App.tsx
+++ examples/SSR/typescript-react/app/App.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import * as React from "react";
 import { Helmet } from "react-helmet";
 //import { Switch, Route, Redirect } from "react-router-dom";
 import Home from "./components/Home";
 import favicon from "./assets/favicon.ico";
 import importedComponent from "react-imported-component";
 
-const Another = importedComponent(() => import("./components/Another"));
+const Another = importedComponent(() => import(/* webpackChunkName: namedChunk */"./components/Another"));
 //import Another from "./components/Another";
 
 export default function App() {
