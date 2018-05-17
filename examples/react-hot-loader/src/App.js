057cb8114592b42e71e4290f6e1d5c99287d98cf
Anton Korzunov <thekashey@gmail.com>
2018-05-17 17:22:49 +1000

    better examples

--- examples/react-hot-loader/src/App.js
+++ examples/react-hot-loader/src/App.js
@@ -4,14 +4,16 @@ import Counter from './Counter'
 import imported from 'react-imported-component'
 import Portal from './Portal'
 
-const Async = imported(() => import('./DeferredRender'));
-const ShouldNotBeImported = imported(() => import('./NotImported'));
+const Async = imported(() => import(/* webpackChunkName: "namedChunk-1" */'./DeferredRender'));
+const Async2 = imported(() => import(/* webpackChunkName: "namedChunk-2" */'./DeferredRender'));
+const ShouldNotBeImported = imported(() => import(/* webpackChunkName: "namedChunk-2" */'./NotImported'));
 
 const App = () => (
   <h1>
     <p>{42}!</p>
     <p>C: <Counter /></p>
-    <p>A: <Async /></p>
+    <p>A1: <Async /></p>
+    <p>A2: <Async2 /></p>
     <p>P: <Portal /></p>
     { Date.now()<0 && <ShouldNotBeImported />}
   </h1>
--- examples/react-hot-loader/webpack.config.babel.js
+++ examples/react-hot-loader/webpack.config.babel.js
@@ -8,6 +8,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
+    chunkFilename: '[name].client.js',
   },
   module: {
     rules: [
