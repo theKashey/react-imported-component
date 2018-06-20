856f9e71004a4d4e0dd3f98646914eeaf522cadb
Anton Korzunov <thekashey@gmail.com>
2018-06-20 17:56:34 +1000

    add flow typings

--- examples/react-hot-loader/src/App.js
+++ examples/react-hot-loader/src/App.js
@@ -1,8 +1,13 @@
-import { hot, setConfig } from 'react-hot-loader'
+// @flow
+import {hot, setConfig} from 'react-hot-loader'
 import * as React from 'react'
 import Counter from './Counter'
-import imported from 'react-imported-component'
+import imported, {ComponentLoader} from 'react-imported-component'
 import Portal from './Portal'
+
+imported(() => import(/* webpackChunkName: "namedChunk-1" */'./DeferredRender'), {
+  async: true
+});
 
 const Async = imported(() => import(/* webpackChunkName: "namedChunk-1" */'./DeferredRender'));
 const Async2 = imported(() => import(/* webpackChunkName: "namedChunk-2" */'./DeferredRender'));
@@ -10,15 +15,18 @@ const ShouldNotBeImported = imported(() => import(/* webpackChunkName: "namedChu
 
 const App = () => (
   <h1>
+    <ComponentLoader
+      loadable={() => import(/* webpackChunkName: "namedChunk-1" */'./DeferredRender')}
+    />
     <p>{42}!</p>
-    <p>C: <Counter /></p>
-    <p>A1: <Async /></p>
-    <p>A2: <Async2 /></p>
-    <p>P: <Portal /></p>
-    { Date.now()<0 && <ShouldNotBeImported />}
+    <p>C: <Counter/></p>
+    <p>A1: <Async/></p>
+    <p>A2: <Async2/></p>
+    <p>P: <Portal/></p>
+    {Date.now() < 0 && <ShouldNotBeImported/>}
   </h1>
 )
 
-setConfig({ logLevel: 'debug' })
+setConfig({logLevel: 'debug'})
 
 export default hot(module)(App)
